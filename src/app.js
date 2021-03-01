953acaa3ecd4c83eb6479485885a0d76be0f1d13
Vishal Kumar <vishal.kumar14@mountblue.tech>
2021-03-01 10:16:30 +0530

    adding text in about.hbs extracting some extra information for weatherstack in util/forecast.js

--- src/app.js
+++ src/app.js
@@ -22,14 +22,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Andrew Mead'
+        name: 'Vishal Kumar'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Andrew Mead'
+        name: 'Vishal kumar'
     })
 })
 
@@ -37,7 +37,7 @@ app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text.',
         title: 'Help',
-        name: 'Andrew Mead'
+        name: 'Vishal kumar'
     })
 })
 
@@ -77,7 +77,7 @@ app.get('/weather', (req, res) => {
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Vishal kumar',
         errorMessage: 'Help article not found.'
     })
 })
@@ -85,7 +85,7 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Vishal Kumar',
         errorMessage: 'Page not found.'
     })
 })
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ' degress out. ' )
+            callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ' degress out. Feel likes temprature is ' + body.current.feelslike + ' degree out and humidity is ' + body.current.humidity + "%." )
         }
        
     })
